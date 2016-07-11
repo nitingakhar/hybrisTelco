bf543efb0a3a996be22cea27a6eb028056b30ca6
nitin_gakhar@epam.com <Jaimatadi@123>
2016-07-11 14:48:04 +0200

    Changes for getting custom message during account lock

--- hybrisCustom/custom-ext/bonstore/bonstorestorefront/web/src/org/bonstore/storefront/controllers/pages/LoginPageController.java
+++ hybrisCustom/custom-ext/bonstore/bonstorestorefront/web/src/org/bonstore/storefront/controllers/pages/LoginPageController.java
@@ -9,15 +9,24 @@
  * Information and shall use it only in accordance with the terms of the
  * license agreement you entered into with hybris.
  *
- *  
+ *
  */
 package org.bonstore.storefront.controllers.pages;
 
+import de.hybris.platform.acceleratorstorefrontcommons.breadcrumb.Breadcrumb;
+import de.hybris.platform.acceleratorstorefrontcommons.controllers.ThirdPartyConstants;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractLoginPageController;
+import de.hybris.platform.acceleratorstorefrontcommons.controllers.util.GlobalMessages;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.GuestForm;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.LoginForm;
 import de.hybris.platform.acceleratorstorefrontcommons.forms.RegisterForm;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
 import de.hybris.platform.cms2.model.pages.AbstractPageModel;
-import org.bonstore.storefront.controllers.ControllerConstants;
+import de.hybris.platform.cms2.model.pages.ContentPageModel;
+import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.servicelayer.user.UserService;
+
+import java.util.Collections;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +34,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.StringUtils;
+import org.bonstore.storefront.controllers.ControllerConstants;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.stereotype.Controller;
@@ -46,6 +56,16 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginPageController extends AbstractLoginPageController
 {
 	private HttpSessionRequestCache httpSessionRequestCache;
+	@Resource(name = "userService")
+	private UserService userService;
+
+	/**
+	 * @return the userService
+	 */
+	public UserService getUserService()
+	{
+		return userService;
+	}
 
 	@Override
 	protected String getView()
@@ -87,6 +107,51 @@ public class LoginPageController extends AbstractLoginPageController
 			storeReferer(referer, request, response);
 		}
 		return getDefaultLoginPage(loginError, session, model);
+	}
+
+	@Override
+	protected String getDefaultLoginPage(final boolean loginError, final HttpSession session, final Model model)
+			throws CMSItemNotFoundException
+	{
+		final LoginForm loginForm = new LoginForm();
+		model.addAttribute(loginForm);
+		model.addAttribute(new RegisterForm());
+		model.addAttribute(new GuestForm());
+
+		final String username = (String) session.getAttribute(SPRING_SECURITY_LAST_USERNAME);
+		if (username != null)
+		{
+			session.removeAttribute(SPRING_SECURITY_LAST_USERNAME);
+		}
+
+		loginForm.setJ_username(username);
+		storeCmsPageInModel(model, getCmsPage());
+		setUpMetaDataForContentPage(model, (ContentPageModel) getCmsPage());
+		model.addAttribute(ThirdPartyConstants.SeoRobots.META_ROBOTS, ThirdPartyConstants.SeoRobots.INDEX_NOFOLLOW);
+
+		final Breadcrumb loginBreadcrumbEntry = new Breadcrumb("#",
+				getMessageSource().getMessage("header.link.login", null, "header.link.login", getI18nService().getCurrentLocale()),
+				null);
+		model.addAttribute("breadcrumbs", Collections.singletonList(loginBreadcrumbEntry));
+
+		CustomerModel customerModel = null;
+		if (loginError)
+		{
+			customerModel = (CustomerModel) getUserService().getUserForUID(StringUtils.lowerCase(username));
+			final boolean isLogin = customerModel.isLoginDisabled();
+
+			model.addAttribute("loginError", Boolean.valueOf(loginError));
+			if (isLogin)
+			{
+				GlobalMessages.addErrorMessage(model, "login.error.account.disabled.message");
+			}
+			else
+			{
+				GlobalMessages.addErrorMessage(model, "login.error.account.not.found.title");
+			}
+		}
+
+		return getView();
 	}
 
 	protected void storeReferer(final String referer, final HttpServletRequest request, final HttpServletResponse response)
